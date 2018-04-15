f47e959fe9aa498351dece9d431f2b6ea6420e96
Explorer <propokerstar90@gmail.com>
2018-04-15 21:49:59 +0300

    Done

--- github.js
+++ github.js
@@ -2,15 +2,22 @@ class Github {
     constructor(){
         this.client_id = '252e6f2530b612003b47';
         this.client_secret = '61b15b7ff46ec819a02994a208d7d87c5c78e2b0';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
 
     async getUser(user){
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}
+        &sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return{
-            profile
+            profile,
+            repos
         }
     }
 }
